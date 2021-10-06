2ceb161a9fe47ba6ffd1c11505b678e93ad44f44
IronMarken <ferrimarco98@gmail.com>
2021-10-06 10:16:40 +0200

    nodeId as SHA1

--- Pastry/pastry.go
+++ Pastry/pastry.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	"crypto/rand"
+	"crypto/sha1"
 	"fmt"
 	"net"
 	"net/http"
@@ -33,19 +33,20 @@ func join(w http.ResponseWriter, req *http.Request) {
 		//Nobody in the network
 	}
 
-	fmt.Fprintf(w, "%s\n", string(nodeId))
+	//fmt.Fprintf(w, "%s\n", string(nodeId))
+	fmt.Fprintf(w, "%s\n",string(metadata.nodeId))
 }
 
-func connectToNode(arg string) {
+func connectToNode(fullAddress string) {
 	//Connect to a node
-	fmt.Println("Connecting to localhost:", arg)
+	fmt.Println("Connecting to : ", fullAddress)
 
-	targetIp := fmt.Sprintf("http://localhost:%s", arg)
+	targetIp := fmt.Sprintf(fullAddress)
 
 
 	//NEED TO SEND JOIN(HASH)
 
-	resp, err := http.Get(fmt.Sprintf("%s/join", targetIp))
+	resp, err := http.Get(fmt.Sprintf("http://%s/join", targetIp))
 	if err != nil {
 		panic(err)
 	}
@@ -55,12 +56,12 @@ func connectToNode(arg string) {
 
 	scanner := bufio.NewScanner(resp.Body)
 	for i := 0; scanner.Scan(); i++ {
-		targetId = scanner.Bytes()
+		targetId = []byte(scanner.Text())
 	}
 
 	targetMetadata := metadataStruct{nodeId: targetId, ipAddress: targetIp}
 
-	fmt.Println("Connected to: ", targetMetadata)
+	fmt.Printf("Connected to fullAddress %s nodeId %x\n", fullAddress, targetMetadata.nodeId)
 
 	if err := scanner.Err(); err != nil {
 		panic(err)
@@ -72,32 +73,46 @@ Routing table
 Leaf set: IP e ID secondo una metrica
 Neighbour set: IP e ID di N nodi vicini
  */
+
+/****************************
+arg[0] -> IP
+arg[1] -> port
+arg[2] -> debug connect/server
+*****************************/
 func main() {
 	//b := 4
 	nodeId = make([]byte, 16)
 
-	rand.Read(nodeId)
-	fmt.Println("NodeId:", nodeId)
+	//remove first one
+	argsWithoutProg := os.Args[1:]
 
-	listener, err := net.Listen("tcp", ":0")
+	ip := argsWithoutProg[0]
+	port := argsWithoutProg[1]
+
+	fullAddress := fmt.Sprintf("%s:%s", ip, port)
+
+	//nodeId with hash
+	h := sha1.New()
+	h.Write([]byte(fullAddress))
+	nodeId := h.Sum(nil)
+
+	fmt.Printf("fullAddress %s nodeId %x\n", fullAddress, nodeId)
+
+	listener, err := net.Listen("tcp", fullAddress)
 	if err != nil {
 		panic(err)
 	}
 
 	metadata.nodeId = nodeId
-	metadata.ipAddress = fmt.Sprintf("http://localhost:%s/nodeid", listener.Addr().(*net.TCPAddr).Port)
+	metadata.ipAddress = fullAddress
 
-	//remove first one
-	argsWithoutProg := os.Args[1:]
 
-	if len(argsWithoutProg) != 0 {
-		connectToNode(argsWithoutProg[0])
+	if len(argsWithoutProg) == 3 {
+		connectToNode(argsWithoutProg[2])
 	}
 
 	//Startup server
 	http.HandleFunc("/join", join)
-
-	fmt.Println("Using port:", listener.Addr().(*net.TCPAddr).Port)
 
 	http.Serve(listener, nil)
 }
