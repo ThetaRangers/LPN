ddbdc2c1081405ca8f3c77a8529cb7b7a3a64608
Lorenzo Valeriani <lorenzoval98@gmail.com>
2021-10-09 17:25:50 +0200

    Ipfs test
    Signed-off-by: Lorenzo Valeriani <lorenzoval98@gmail.com>

---
+++ ipfs/dht.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"sync"
+
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-kad-dht"
+	"github.com/multiformats/go-multiaddr"
+)
+
+func NewDHT(ctx context.Context, host host.Host, bootstrapPeers []multiaddr.Multiaddr) (*dht.IpfsDHT, error) {
+	var options []dht.Option
+
+	if len(bootstrapPeers) == 0 {
+		options = append(options, dht.Mode(dht.ModeServer))
+	}
+
+	ipfsDHT, err := dht.New(ctx, host, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = ipfsDHT.Bootstrap(ctx); err != nil {
+		return nil, err
+	}
+
+	var wg sync.WaitGroup
+	for _, peerAddr := range bootstrapPeers {
+		peerInfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := host.Connect(ctx, *peerInfo); err != nil {
+				log.Printf("Error while connecting to node %q: %-v", peerInfo, err)
+			} else {
+				log.Printf("Connection established with bootstrap node: %q", *peerInfo)
+			}
+		}()
+	}
+	wg.Wait()
+
+	return ipfsDHT, nil
+}
